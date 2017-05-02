5a56afc5a62d6129968c5fa26bb6fc1e2f9f091f
Brenton Cleeland <brenton@brntn.me>
2017-05-02 11:46:39 +1000

    Add Django 1.11

--- djver/djver.py
+++ djver/djver.py
@@ -37,16 +37,21 @@ THIRD_PARTY_CSS = [
 ]
 
 ADMIN_CHANGES = [
-    ('1.10.2-1.10', 'base.css', 'color: #000;'),
-    ('1.9.10-1.9', 'widgets.css', 'margin-left: 7px;'),
-    ('1.8.15-1.8.2', 'forms.css', 'clear: left;'),
-    ('1.8.1', 'widgets.css', '.related-widget-wrapper {'),
-    ('1.8', 'widgets.css', 'opacity: 1;'),
-    ('1.7.9-1.7', 'base.css', '#branding a:hover {'),
-    ('1.6.11-1.6', 'widgets.css', 'width: 360px;'),
-    ('1.5.12-1.5', 'widgets.css', '.url a {'),
-    ('1.4.21-1.4.1', 'widgets.css', '.inline-group .aligned .selector .selector-filter label {'),
-    ('1.4', 'widgets.css', 'background: #e1e1e1 url(../img/nav-bg.gif) top left repeat-x;'),
+    ('1.11', 'css/base.css', 'background-position: right 7px center;'),
+    ('1.10.7-1.10', 'css/base.css', 'color: #000;'),
+    ('1.9.13-1.9', 'css/widgets.css', 'margin-left: 7px;'),
+    ('1.8.18-1.8.2', 'css/forms.css', 'clear: left;'),
+    ('1.8.1', 'css/widgets.css', '.related-widget-wrapper {'),
+    ('1.8', 'css/widgets.css', 'opacity: 1;'),
+    ('1.7.11-1.7', 'css/base.css', '#branding a:hover {'),
+    ('1.6.11-1.6', 'css/widgets.css', 'width: 360px;'),
+    ('1.5.12-1.5', 'css/widgets.css', '.url a {'),
+    ('1.4.21-1.4.18', 'css/login.css', 'text-align: center;'),
+    ('1.4.17', 'css/widgets.css', 'padding: 3px 4px;'),
+    ('1.4.16-1.4.13', 'css/login.css', '404: Not Found'),
+    ('1.4.12', 'css/widgets.css', '404: Not Found'),
+    ('1.4.11-1.4.1', 'css/widgets.css', '.inline-group .aligned .selector .selector-filter label {'),
+    ('1.4', 'css/widgets.css', 'background: #e1e1e1 url(../img/nav-bg.gif) top left repeat-x;'),
 ]
 
 
@@ -72,23 +77,35 @@ def check_version(base_url, static_path, verbose=False):
         base_url = 'http://{}'.format(base_url)
 
     for version, path, string in ADMIN_CHANGES:
-        url = '{}{}admin/css/{}'.format(base_url, static_path, path)
+        url = '{}{}admin/{}'.format(base_url, static_path, path)
         if check_str(url, string, verbose):
             return version
 
 
 def find_diffs():
     versions = [
-        '1.10.2', '1.10.1', '1.10',
-        '1.9.10', '1.9.9', '1.9.8', '1.9.7', '1.9.6', '1.9.5', '1.9.4', '1.9.3', '1.9.2', '1.9.1', '1.9',
-        '1.8.15', '1.8.14', '1.8.13', '1.8.12', '1.8.11', '1.8.10', '1.8.9', '1.8.8', '1.8.7', '1.8.6', '1.8.5', '1.8.4', '1.8.3', '1.8.2', '1.8.1', '1.8',
-        '1.7.9', '1.7.8', '1.7.7', '1.7.6', '1.7.5', '1.7.4', '1.7.3', '1.7.2', '1.7.1', '1.7',
+        '1.11',
+
+        '1.10.7', '1.10.6', '1.10.5', '1.10.4', '1.10.3', '1.10.2', '1.10.1', '1.10',
+
+        '1.9.13', '1.9.12', '1.9.11', '1.9.10', '1.9.9', '1.9.8', '1.9.7', '1.9.6', '1.9.5', '1.9.4', '1.9.3', '1.9.2',
+        '1.9.1', '1.9',
+
+        '1.8.18', '1.8.17', '1.8.16', '1.8.15', '1.8.14', '1.8.13', '1.8.12', '1.8.11', '1.8.10', '1.8.9', '1.8.8',
+        '1.8.7', '1.8.6', '1.8.5', '1.8.4', '1.8.3', '1.8.2', '1.8.1', '1.8',
+
+        '1.7.11', '1.7.10', '1.7.9', '1.7.8', '1.7.7', '1.7.6', '1.7.5', '1.7.4', '1.7.3', '1.7.2', '1.7.1', '1.7',
+
         '1.6.11', '1.6.10', '1.6.9', '1.6.8', '1.6.7', '1.6.6', '1.6.5', '1.6.4', '1.6.3', '1.6.2', '1.6.1', '1.6',
-        '1.5.12', '1.5.11', '1.5.10', '1.5.9', '1.5.8', '1.5.7', '1.5.6', '1.5.5', '1.5.4', '1.5.3', '1.5.2', '1.5.1', '1.5',
-        '1.4.21', '1.4.20', '1.4.19', '1.4.18', '1.4.17', '1.4.16', '1.4.15', '1.4.14', '1.4.13', '1.4.12', '1.4.11', '1.4.10',
-        '1.4.9', '1.4.8', '1.4.7', '1.4.6', '1.4.5', '1.4.4', '1.4.3', '1.4.2', '1.4.1', '1.4',
+
+        '1.5.12', '1.5.11', '1.5.10', '1.5.9', '1.5.8', '1.5.7', '1.5.6', '1.5.5', '1.5.4', '1.5.3', '1.5.2', '1.5.1',
+        '1.5',
+
+        '1.4.21', '1.4.20', '1.4.19', '1.4.18', '1.4.17', '1.4.16', '1.4.15', '1.4.14', '1.4.13', '1.4.12', '1.4.11',
+        '1.4.10', '1.4.9', '1.4.8', '1.4.7', '1.4.6', '1.4.5', '1.4.4', '1.4.3', '1.4.2', '1.4.1', '1.4',
+
         '1.3.7', '1.3.6', '1.3.5', '1.3.4', '1.3.3', '1.3.2', '1.3.1', '1.3',
-        ]
+    ]
 
     # favour files _not_ found in django-flat-theme
     files = [
@@ -109,7 +126,8 @@ def find_diffs():
         # "js/urlify.js",
 
         # admin css
-        'css/dashboard.css', 'css/widgets.css', 'css/base.css', 'css/forms.css', 'css/login.css', 'css/dashboard.css',
+        # 'css/dashboard.css', 
+        'css/widgets.css', 'css/base.css', 'css/forms.css', 'css/login.css', 'css/dashboard.css',
         # 'css/ie.css',  # removed in 1.9.x
     ]
 
@@ -120,9 +138,10 @@ def find_diffs():
         for fn in files:
             full_path = 'files/{}/{}'.format(v, fn)
             if not os.path.exists(full_path):
-                url = 'https://raw.githubusercontent.com/django/django/{}/django/contrib/admin/static/admin/{}'.format(v, fn)
+                repo = 'https://raw.githubusercontent.com/django/django/'
+                url = '{}{}/django/contrib/admin/static/admin/{}'.format(repo, v, fn)
                 if v.startswith('1.3'):
-                    url = 'https://raw.githubusercontent.com/django/django/{}/django/contrib/admin/media/{}'.format(v, fn)
+                    url = '{}{}/django/contrib/admin/media/{}'.format(repo, v, fn)
                 response = requests.get(url)
 
                 print('[{}] {}'.format(response.status_code, url))
@@ -132,7 +151,7 @@ def find_diffs():
     matched_versions = []
     for i, v1 in enumerate(versions[:-1]):
         matched_versions.append(v1)
-        v2 = versions[i+1]
+        v2 = versions[i + 1]
         new_line = None
 
         for f in files:
@@ -148,7 +167,7 @@ def find_diffs():
                     # ensure this line is unique within the file
                     if f1.count(line) == 1:
                         # we also want to make sure that it doesn't appear in any _older_ versions
-                        for v in versions[i+1:]:
+                        for v in versions[i + 1:]:
                             f3 = open('files/{}/{}'.format(v, f)).read()
                             if line in f3:
                                 break
